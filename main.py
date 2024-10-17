b60c7989922ff9ba8804267a9020c9c99db2cf44
Hoang Chi Phi <phi.220236@tbd.edu.vn>
2024-10-17 11:17:09 +0700

    game function

--- main.py
+++ main.py
@@ -2,6 +2,69 @@ import pygame as pg
 import sys, random
 
 pg.mixer.pre_init(frequency=44100, size=-16, channels=2, buffer=512)
+#game function
+def draw_floor():
+    screen.blit(floor,(floor_x_pos,650))
+    screen.blit(floor,(floor_x_pos+432,650))
+
+def create_pipe():
+    random_pipe_pos = random.choice(pipe_height)
+    top_pipe = pipe_surface.get_rect(midtop = (500,random_pipe_pos-650))
+
+    bot_pipe = pipe_surface.get_rect(midtop = (500,random_pipe_pos))
+    return bot_pipe, top_pipe
+
+def move_pipe(pipes):
+    for pipe in pipes:
+        pipe.centerx -=5
+    return pipes
+
+def draw_pipe(pipes):
+    for pipe in pipes:
+        if pipe.bottom >=384:        
+            screen.blit(pipe_surface, pipe)
+        else:
+            flip_pipe = pg.transform.flip(pipe_surface, False, True)
+            screen.blit(flip_pipe, pipe)
+
+def check_collision(pipes):
+    for pipe in pipes:
+        if bird_rect.colliderect(pipe):
+            hit_sound.play()
+            return False
+    if bird_rect.top <= -75 or bird_rect.bottom >= 650:
+        return False
+    return True
+    
+def rotate_bird(bird1):
+    new_bird = pg.transform.rotozoom(bird1,-bird_move*3,1)
+    return new_bird
+
+def bird_animation():
+
+    new_bird = bird_list[bird_index]
+    new_bird_rect = new_bird.get_rect(center=(100,bird_rect.centery))
+    return new_bird, new_bird_rect
+
+def score_display(game_state):
+    if game_state == 'main game':
+        score_surface= game_font.render(f'Score: {int(score)}', True,(255,255,255))
+        score_rect = score_surface.get_rect(center=(216,100))
+        screen.blit(score_surface, score_rect)
+    if game_state == 'game over':
+        score_surface= game_font.render(f'Score: {int(score)}', True,(255,255,255))
+        score_rect = score_surface.get_rect(center=(216,100))
+        screen.blit(score_surface, score_rect)
+
+        high_score_surface = game_font.render(f'High score: {int(high_score)}', True,(255,255,255))
+        high_score_rect = high_score_surface.get_rect(center=(216,630))
+        screen.blit(high_score_surface, high_score_rect)
+
+def update_score(score, high_score):
+    if score > high_score:
+        high_score = score
+    return high_score
+
 
 pg.init()
 #game parameter
